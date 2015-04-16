a2216c9e6863a2ee7e98e2b9c8a3505c90ceb55f
Alan Reyes <kutt@katrea.net>
2015-04-16 11:39:23 -0500

    Initial support for split screen output

--- lib/manager.py
+++ lib/manager.py
@@ -42,7 +42,7 @@ def create_target_view_for_source_view(view, type):
 
     if source_id not in _target_views_by_svid:
         if type == 'buffer':
-            new_view = view.window().new_file()
+            new_view = _create_view_in_target_group(view)
         else:
             vid = 'toolrunner-output:' + source_id
             new_view = view.window().create_output_panel(vid)
@@ -55,6 +55,97 @@ def create_target_view_for_source_view(view, type):
         _svids_by_tvid[target_id] = source_id
 
     return _target_views_by_svid[source_id]
+
+def _create_view_in_target_group(view):
+    win = view.window()
+    group, idx = win.get_view_index(view)
+
+    target_group = settings.get_setting('output_tab_position')
+    if target_group not in set(['top', 'bottom', 'left', 'right']):
+        target = group
+    else:
+        layout = win.get_layout()
+        
+
+        x1 = 0
+        y1 = 1
+        x2 = 2
+        y2 = 3
+
+        origin_coords = layout['cells'][group]
+        debug.log(origin_coords)
+        min_y = 0
+        min_x = 0
+        max_target = None
+        min_target = None
+
+        for idx in range(0, len(layout['cells'])):
+            if idx == group:
+                continue
+            tgroup = layout['cells'][idx]
+
+            if tgroup[y1] == origin_coords[y2]:
+                debug.log('Y-Matches', idx, tgroup)
+                if tgroup[x1] >= origin_coords[x1]:
+                    if max_target == None or tgroup[x1] < max_target:
+                        debug.log('X Max Matches: ', idx, tgroup)
+                        max_target = idx
+                if tgroup[x1] <= origin_coords[x1]:
+                    if min_target == None or tgroup[x1] > min_target:
+                        debug.log('X Min Matches: ', idx, tgroup)
+                        min_target = idx
+
+        debug.log('Target: %s, %s' % (max_target, min_target))
+
+        target = max_target or min_target
+
+        if target is None:
+            cells = layout['cells']
+
+            new_cells = list()
+            for cell in cells:
+                new_cells.append([layout['cols'][cell[x1]], layout['rows'][cell[y1]], layout['cols'][cell[x2]], layout['rows'][cell[y2]]])
+
+            current_cell = new_cells[group]
+
+            debug.log(new_cells, current_cell)
+
+            new_cell= list(current_cell)
+
+            nc_height = (current_cell[y2] - current_cell[y1]) / 3
+            dic_y = current_cell[y2] - nc_height
+
+            new_cell[y1] = dic_y
+            current_cell[y2] = dic_y
+
+            new_cells.append(new_cell)
+
+            layout['rows'].append(dic_y)
+            rows = list(sorted(set(layout['rows'])))
+            
+            layout['rows'] = rows
+
+            for cell in new_cells:
+                cell[x1] = layout['cols'].index(cell[x1])
+                cell[y1] = layout['rows'].index(cell[y1])
+                cell[x2] = layout['cols'].index(cell[x2])
+                cell[y2] = layout['rows'].index(cell[y2])
+
+            debug.log("New cells", new_cells)
+
+            layout['cells'] = new_cells
+
+            target = len(new_cells) - 1
+
+            win.set_layout(layout)
+
+        if target is not None:
+            win.focus_group(target)
+
+    target_view = win.new_file()
+    win.focus_group(group)
+
+    return target_view
 
 def get_source_view_id_for_target_view_id(view_id):
     view_id = str(view_id)
