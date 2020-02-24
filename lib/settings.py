75d53a3ee72c491e74ef2717e4df27f970331f10
Alan Reyes <kutt@katrea.net>
2020-02-23 20:15:58 -0600

    Replaces the internal manage of settings for the Better Settings dependency

--- lib/settings.py
+++ lib/settings.py
@@ -1,99 +1,33 @@
-import sublime
-
 import platform
 import re
 
+import sublime
+import better_settings
+
 from . import debug
-
-basepackage = re.sub(r"\.lib$", "", __package__)
-
-hostplatform_settings = None
-host_settings = None
-platform_settings = None
-user_settings = None
 
 _tool_list = None
 _tool_map = None
 
 _plugin_loaded = False
 _on_plugin_loaded_callbacks = list()
-
-
-def get_hostplatform_settings_filename():
-    return get_settings_filename(
-        platform.uname()[1].lower() + " on " + sublime.platform().capitalize())
-
-
-def get_host_settings_filename():
-    return get_settings_filename(
-        platform.uname()[1].lower())
-
-
-def get_platform_settings_filename():
-    return get_settings_filename(sublime.platform().capitalize())
-
-
-def get_settings_filename(special=None):
-    special = " (" + special + ")" if special else ""
-    return "".join(("ToolRunner", special, ".sublime-settings"))
+_settings = None
 
 
 def get_setting(setting_name, default=None):
-    return hostplatform_settings.get(
-        setting_name,
-        host_settings.get(
-            setting_name,
-            platform_settings.get(
-                setting_name,
-                user_settings.get(setting_name, default)
-            )
-        )
-    )
+    return _settings.get(setting_name, default)
 
 
-def get_hostplatform_setting(setting_name, default=None):
-    return hostplatform_settings.get(setting_name, default)
-
-
-def get_host_setting(setting_name, default=None):
-    return host_settings.get(setting_name, default)
-
-
-def get_platform_setting(setting_name, default=None):
-    return platform_settings.get(setting_name, default)
-
-
-def get_user_setting(setting_name, default=None):
-    return user_settings.get(setting_name, default)
-
-
-def set_setting(setting_name, settingValue):
-    hostplatform_settings.set(setting_name, settingValue)
-    sublime.save_settings(get_hostplatform_settings_filename())
-
-
-def get_settings_file_path(scope):
-    return "${packages}/%0s/%1s" % get_settings_pieces(scope)
-
-
-def get_settings_pieces(scope):
-    if scope == 'host/os':
-        return ('User/', get_hostplatform_settings_filename())
-    if scope == 'host':
-        return ('User/', get_host_settings_filename())
-    elif scope == 'user':
-        return ('User/', get_settings_filename())
-    elif scope == 'os':
-        return ('User/', get_platform_settings_filename())
-    else:  # default
-        return (basepackage, get_settings_filename())
+def set_setting(setting_name, setting_value):
+    _settings.set(better_settings.SCOPE_HOST, setting_name, setting_value)
+    _settings.save()
 
 
 def get_groups():
-    groups = get_hostplatform_setting('user_groups', [])
-    groups += get_host_setting('user_groups', [])
-    groups += get_platform_setting('user_groups', [])
-    groups += get_user_setting('user_groups', [])
+    groups = _settings.get_hostplatform_setting("user_groups", [])
+    groups += _settings.get_host_setting("user_groups", [])
+    groups += _settings.get_platform_setting("user_groups", [])
+    groups += _settings.get_user_setting("user_groups", [])
 
     return groups
 
@@ -119,7 +53,7 @@ def get_tool(tool_id):
 
 
 def get_override(tool_id):
-    return get_setting('user_tool_overrides', {}).get(tool_id)
+    return _settings.get("user_tool_overrides", {}).get(tool_id)
 
 
 def _build_tool_list():
@@ -129,14 +63,14 @@ def _build_tool_list():
     _tool_list = []
 
     for settings_set in (
-        get_hostplatform_setting('user_tools', []),
-        get_host_setting('user_tools', []),
-        get_platform_setting('user_tools', []),
-        get_user_setting('user_tools', []),
-        get_user_setting('default_tools', [])
+        _settings.get_hostplatform_setting("user_tools", []),
+        _settings.get_host_setting("user_tools", []),
+        _settings.get_platform_setting("user_tools", []),
+        _settings.get_user_setting("user_tools", []),
+        _settings.get_user_setting("default_tools", []),
     ):
         for tool_item in settings_set:
-            key = tool_item.get('name', tool_item.get('cmd'))
+            key = tool_item.get("name", tool_item.get("cmd"))
 
             if key is None:
                 debug.log("Tool has no cmd: ", tool_item)
@@ -149,40 +83,29 @@ def _build_tool_list():
             if key not in _tool_map:
                 override_cmd = get_override(tool_item['name'])
                 if override_cmd is not None:
-                    tool_item['cmd'] = override_cmd
+                    tool_item["cmd"] = override_cmd
 
                 _tool_map[key] = tool_item
                 _tool_list.append(tool_item)
 
 
 def on_loaded():
-    global hostplatform_settings, host_settings, platform_settings, user_settings
     global _plugin_loaded
+    global _settings
 
     if _plugin_loaded:
         debug.log("Plugin already loaded")
         return
 
-    hostplatform_settings = sublime.load_settings(
-        get_hostplatform_settings_filename())
+    basepackage = re.sub(r"\.lib$", "", __package__)
 
-    host_settings = sublime.load_settings(
-        get_host_settings_filename())
-
-    platform_settings = sublime.load_settings(
-        get_platform_settings_filename())
-
-    user_settings = sublime.load_settings(
-        get_settings_filename())
+    _settings = better_settings.load_for(basepackage, "ToolRunner")
 
     on_debug_change()
 
-    debug.log('Registering Settings Callbacks')
+    debug.log("Registering Settings Callbacks")
 
-    hostplatform_settings.add_on_change('debug', on_debug_change)
-    user_settings.add_on_change('debug', on_debug_change)
-    platform_settings.add_on_change('debug', on_debug_change)
-    host_settings.add_on_change('debug', on_debug_change)
+    _settings.add_on_change("debug", on_debug_change)
 
     if _on_plugin_loaded_callbacks is not None:
         for callback in _on_plugin_loaded_callbacks:
@@ -193,23 +116,12 @@ def on_loaded():
 
 
 def on_unloaded():
-    if hostplatform_settings is not None:
-        hostplatform_settings.clear_on_change('debug')
-
-    if host_settings is not None:
-        host_settings.clear_on_change('debug')
-
-    if platform_settings is not None:
-        platform_settings.clear_on_change('debug')
-
-    if user_settings is not None:
-        user_settings.clear_on_change('debug')
-
+    _settings.clear_on_change("debug")
     del _on_plugin_loaded_callbacks[:]
 
 
 def on_debug_change():
-    debug.enabled = get_setting('debug')
+    debug.enabled = _settings.get("debug")
 
 
 def register_on_plugin_loaded(callback):
@@ -217,3 +129,6 @@ def register_on_plugin_loaded(callback):
         callback()
     else:
         _on_plugin_loaded_callbacks.append(callback)
+
+def open_settings(window, scope):
+    _settings.open_settings(window, scope)
--- lib/util.py
+++ lib/util.py
@@ -1,10 +1,10 @@
+import re
+from os import path
+
 import sublime
 
-from os import path
-import re
+from . import debug, settings
 
-from . import debug
-from . import settings
 
 def expand(value, view):
     if value is None:
@@ -17,21 +17,24 @@ def expand(value, view):
 
     return expanded
 
+
 def expand_variables(str, vars):
     try:
         return sublime.expand_variables(str, vars)
     except AttributeError as e:
+
         def repl(match):
             replacement = vars.get(match.group(1), None)
 
             if replacement is None:
                 default = match.group(2)
                 if default is not None:
-                    return expand_variables(default,vars)
+                    return expand_variables(default, vars)
 
             return replacement
 
-        return re.sub(r'\${([\w-]+)(?:[^}]*)?}', repl, str)
+        return re.sub(r"\${([\w-]+)(?:[^}]*)?}", repl, str)
+
 
 def extract_variables(view):
     win = view.window()
@@ -39,44 +42,50 @@ def extract_variables(view):
         return win.extract_variables()
     except AttributeError as e:
         filename = view.file_name()
-        folder, basename = path.split(filename) if filename is not None else (None, None)
+        folder, basename = (
+            path.split(filename) if filename is not None else (None, None)
+        )
         base, ext = path.splitext(basename) if filename is not None else (None, None)
 
         project = win.project_file_name()
-        pfolder, pbasename = path.split(project) if project is not None else (None, None)
-        basep, baseext = path.splitext(path.basename(project)) if project is not None else (None, None)
+        pfolder, pbasename = (
+            path.split(project) if project is not None else (None, None)
+        )
+        basep, baseext = (
+            path.splitext(path.basename(project))
+            if project is not None
+            else (None, None)
+        )
 
         return {
-            'packages': sublime.packages_path(),
-            'platform': sublime.platform(),
-            'file': filename,
-            'file_path': folder,
-            'file_name': basename,
-            'file_base_name': base,
-            'file_extension': ext,
-            'folder': folder,
-            'project': project,
-            'project_path': pfolder,
-            'project_name': pbasename,
-            'project_base_name': basep,
-            'project_extension': baseext
+            "packages": sublime.packages_path(),
+            "platform": sublime.platform(),
+            "file": filename,
+            "file_path": folder,
+            "file_name": basename,
+            "file_base_name": base,
+            "file_extension": ext,
+            "folder": folder,
+            "project": project,
+            "project_path": pfolder,
+            "project_name": pbasename,
+            "project_base_name": basep,
+            "project_extension": baseext,
         }
 
 
 def notify(msg, desc=None, source=None, target=None):
     if desc is None:
-        desc = 'ToolRunner'
+        desc = "ToolRunner"
     else:
-        desc = 'ToolRunner[%s]' % desc
+        desc = "ToolRunner[%s]" % desc
 
     debug.log(msg)
 
     if source is None:
         source = sublime.active_window().active_view()
 
-    source.set_status(
-        "toolrunner", "%s: %s" % (desc, msg))
+    source.set_status("toolrunner", "%s: %s" % (desc, msg))
 
     if target is not None:
-        target.set_status(
-            "toolrunner", "%s: %s" % (desc, msg))
+        target.set_status("toolrunner", "%s: %s" % (desc, msg))
